c3b3fca9c511891491aa5709e04907def6b67319
Roberto Kemps <kempsamorim121@gmail.com>
2025-09-08 21:57:46 -0300

    att

--- A3/Radar.E29.py
+++
@@ -1,9 +0,0 @@
-# .29 ESCREVA UM PROGRAMA QUE LEIA A VELOCIDADE DE UM VEICULO. SE ULTRAPASSAR 80KM/H MOSTRE UMA MENSSAGEM DIZENDO QUE FOI MULTADO. A MULTA VAI CUSTA R$7,00 POR CADA KM ACIMA DO LIMITE
-velocidade =int(input('Qual a velocidade atual do veiculo ?'))
-if velocidade > 80:
-    print(f'VOCÊ ULTAPASSOU O LIMITE DE 80KM/H')
-    print(f'VOCÊ SERÁ MULTADO EM R${(velocidade - 80) * 7:.2f}')
-else:
-    print('Tenha um bom dia, dirija com segurança!')
-
-
