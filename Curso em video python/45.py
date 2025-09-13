208bbc390968fed7ece97296e5f032b111c4b939
Roberto Kemps <kempsamorim121@gmail.com>
2025-09-13 04:29:59 -0300

    att jokenpô

--- Curso em video python/45.py
+++ Curso em video python/45.py
@@ -1,23 +1,32 @@
 import random
 from time import sleep
-n = int(input('''[1] PEDRA
-[2] PAPEL
-[3] TESOURA
-Esolha um numero de 1 a 3:'''))
+print('=' * 10, "jokenpô", '=' * 10)
+jogador = str(input('''PEDRA
+PAPEL
+TESOURA
+Esolha:''')).upper()
 #opcao = {1: 'PEDRA', 2: 'PAPEL', 3: 'TESOURA', }
-if n == 1:
-    print('Você escolheu: PEDRA')
-elif n == 2:
-    print('Você escolheu: PAPEL')
-elif n == 3:
-    print('Você escolheu: TESOURA')
+if jogador == 'PEDRA' or jogador == 'PAPEL' or jogador == 'TESOURA':
+    print('Sua escolha foi:', jogador)
 else:
-    print("Valor invalido tente novamente!")
-
-jokenpo = random.randint(1,3)
-print(opcao[jokenpo])
-if n == opcao[jokenpo]:
-    print('empate')
+    print('COMANDO INVALIDO, TENTE NOVAMENTE!! pedra, papel ou tesoura')
+computador = random.choice(["pedra","papel","tesoura"]).upper()
+print(f'Computador escolheu: {computador}')
+if jogador == computador:
+    print("Empate")
+elif jogador == "PEDRA" and computador == 'PAPEL':
+    print("Computador ganhou!")
+elif jogador == "PAPEL" and computador == 'TESOURA':
+    print("Computador ganhou!")
+elif jogador == 'TESOURA' and computador == 'PEDRA':
+    print('Computador ganhou!')
+else:
+    if computador == 'PEDRA' and jogador == 'PAPEL':
+        print('Você ganhou!')
+    elif computador == 'PAPEL' and jogador == 'TESOURA':
+        print('Você ganhou!')
+    elif computador == 'TESOURA' and jogador == 'PEDRA':
+        print('Você ganhou!')
 
 
 
