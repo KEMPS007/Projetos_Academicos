c26e5c566705a30c35a181de7b08ac5d5262b7dc
Roberto Kemps <kempsamorim121@gmail.com>
2025-09-14 21:42:57 -0300

    att

---
+++ Curso em video Java/OlaMundoSwing/src/OlaMundoJava.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+public class OlaMundoJava {
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        frame.setSize(420,420);
+        frame.setVisible(true);
+
+    }
+}
---
+++ Curso em video Java/teste/primeiroprogama/HoraDoSistema.java
@@ -0,0 +1,10 @@
+import java.util.Locale;
+
+//import java.util.Locale;
+public class HoraDoSistema {
+    public static void main(String[] args){
+        Locale locale = Locale.getDefault();
+        System.out.print(locale);
+
+    }
+}
