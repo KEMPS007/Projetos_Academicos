94787c2080c849d404ba69385b62e1710105c940
Roberto Kemps <kempsamorim121@gmail.com>
2025-10-28 17:26:07 -0300

    main

---
+++ O curso definitivo de Lógica de Programação. Do VisualG às principais linguagens do mercado C, C++, Python, C# e Java/JAVA/Atividaes/idades.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class idades {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		String nome1,nome2;
+		int idade1,idade2;		
+		double media;
+		
+		System.out.println("Dados da primera pessoa");
+		System.out.print("Nome: ");
+		nome1 = sc.nextLine();
+		System.out.print("Idade: ");
+		idade1 = sc.nextInt();
+		
+		
+		System.out.println("Dados da segunda pessoa ");
+		sc.nextLine();
+		System.out.print("Nome: ");
+		nome2 = sc.nextLine();
+		System.out.print("idade: ");
+		idade2 = sc.nextInt();
+		
+		media = (double)(idade1 + idade2)/2;
+		
+		System.out.print("A idade media de "+nome1+ " e " + nome2 + " de "+ media+ " anos ");
+		
+		sc.close();
+		
+
+	}
+
+}
