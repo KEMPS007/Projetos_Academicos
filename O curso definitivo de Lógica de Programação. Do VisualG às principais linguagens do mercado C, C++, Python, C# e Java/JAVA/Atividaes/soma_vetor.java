4e8871bf5f35c9c7ff707b255782b733de6d03d1
Roberto Kemps <kempsamorim121@gmail.com>
2025-10-30 07:49:11 -0300

    soma_vetores

---
+++ O curso definitivo de Lógica de Programação. Do VisualG às principais linguagens do mercado C, C++, Python, C# e Java/JAVA/Atividaes/soma_vetor.java
@@ -0,0 +1,42 @@
+public import java.util.Scanner;
+public class soma_vetor {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n,m,i,soma;
+		float media;
+		
+		
+		System.out.print("Quantos numeros voce vai digitar: ");
+		n = sc.nextInt();
+		
+		int [] vet = new int [n];
+		
+		soma = 0;
+		int cont1 = 0;
+		for (i = 0; i < n; i++) {
+			System.out.print("Digete um numero: ");
+			vet[i] = sc.nextInt();
+			soma = soma + vet[i];
+			cont1 = cont1 + 1;
+		}
+		
+		media = (float)soma / cont1;
+		
+		System.out.print("VALORES: ");		
+		for(i = 0; i < n; i++){
+			System.out.print(" "+vet[i]);
+		}
+		System.out.println();
+		System.out.println("soma = " + soma);
+		System.out.println("MEDIA = "+ media);
+		
+		sc.close();
+	}
+
+}
+ {
+    
+}
--- O curso definitivo de Lógica de Programação. Do VisualG às principais linguagens do mercado C, C++, Python, C# e Java/JAVA/Atividaes/tabuada.java
+++ O curso definitivo de Lógica de Programação. Do VisualG às principais linguagens do mercado C, C++, Python, C# e Java/JAVA/Atividaes/tabuada.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 public class tabuada {
-
+//d
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		
-		int x,i,soma;
-		
-		System.out.print("Deseja tabuada para qual valor?: ");
-		x = sc.nextInt();
-		
-		for(i=1; i<11 ; i++){
-			soma = x * i;
-			System.out.println(x+" x "+ i+" = "+soma);
-			
-		}
-		
-		sc.close();
+		try (Scanner sc = new Scanner(System.in)) {
+            int x,i,soma;
+            
+            System.out.print("Deseja tabuada para qual valor?: ");
+            x = sc.nextInt();
+            
+            for(i=1; i<11 ; i++){
+            	soma = x * i;
+            	System.out.println(x+" x "+ i+" = "+soma);
+            	
+            }
+            
+            sc.close();
+        }
 		
 	}
 
